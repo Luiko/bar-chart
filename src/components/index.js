b819b856805854b9cf08270e72a7bd3fdbe8130c
Luiko <lokoluis_box@hotmail.com>
2017-12-21 00:48:38 -0700

    fix chart and axes

--- src/components/index.js
+++ src/components/index.js
@@ -10,30 +10,51 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    const yScale = d3.scaleLinear().domain([0, 18000]).range([0, 400]);
-    const yAxis = d3.axisLeft(yScale);
-    const fromDate = new Date(dataset['form_date']);
+    const marginY = 20;
+    const marginLeft = 40;
+    const yScale = d3.scaleLinear().domain([0, 18000]).range([
+      0,
+      400 - marginY*2])
+    ;
+    const yAxis = d3.axisLeft(d3.scaleLinear().domain([0, 18000]).range([
+      400 - marginY,
+      marginY]))
+    ;
+    const fromDate = new Date(dataset['from_date']);
     const toDate = new Date(dataset['to_date']);
-    const width = Math.ceil(800 / dataset.data.length);
-    const xScale = d3.scaleLinear().domain([fromDate, toDate]).range(0, width * dataset.data.length);
+    const barWidth = Math.floor(900 / dataset.data.length);
+    const xScale = d3.scaleTime().domain([fromDate, toDate]).range([
+      marginLeft,
+      barWidth * dataset.data.length])
+    ;
     const xAxis = d3.axisBottom(xScale);
     d3.select('svg')
-      .append('g').call(yAxis)
+      .append('g')
+      .attr('transform', () => `translate(0,${380})`)
+      .call(xAxis)
+    ;
+    d3.select('svg')
+      .append('g')
+      .attr('transform', () => `translate(${marginLeft},0)`)
+      .call(yAxis)
+    ;
+    console.log(yScale);
+    d3.select('svg')
       .selectAll('rect')
       .data(dataset.data)
       .enter()
       .append('rect')
         .style('fill', 'blue')
-        .attr('width', width)
+        .attr('width', barWidth)
         .attr('height', d => yScale(d[1]))
-        .attr('x', (d, i) => i * width)
-        .attr('y', d => 400 - yScale(d[1]))
+        .attr('x', (d, i) => i * barWidth + marginLeft)
+        .attr('y', d => (400 - marginY) - yScale(d[1]))
     ;
   }
 
   render() {
     return (
-      <svg height='400px' width='800px'></svg>
+      <svg height='400px' width='900px'></svg>
     );
   }
 }
