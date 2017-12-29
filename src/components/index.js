b32fed7728c5c2a24b84ff866a82629d3ab9b32a
Luiko <lokoluis_box@hotmail.com>
2017-12-29 14:52:59 -0700

    fix x axis, was a bit short

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import dataset from "../dataset";
 import * as d3 from 'd3';
 
-
-
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -14,7 +12,7 @@ export default class App extends Component {
     const marginLeft = 40;
     const yScale = d3.scaleLinear().domain([0, 18000]).range([
       0,
-      400 - marginY*2])
+      400 - (marginY * 2)])
     ;
     const yAxis = d3.axisLeft(d3.scaleLinear().domain([0, 18000]).range([
       400 - marginY,
@@ -22,10 +20,10 @@ export default class App extends Component {
     ;
     const fromDate = new Date(dataset['from_date']);
     const toDate = new Date(dataset['to_date']);
-    const barWidth = Math.floor(900 / dataset.data.length);
+    const barWidth = Math.floor((800 + (marginLeft * 2)) / dataset.data.length);
     const xScale = d3.scaleTime().domain([fromDate, toDate]).range([
       marginLeft,
-      barWidth * dataset.data.length])
+      marginLeft + (barWidth * dataset.data.length)])
     ;
     const xAxis = d3.axisBottom(xScale);
     d3.select('svg')
@@ -38,7 +36,6 @@ export default class App extends Component {
       .attr('transform', () => `translate(${marginLeft},0)`)
       .call(yAxis)
     ;
-    console.log(yScale);
     d3.select('svg')
       .selectAll('rect')
       .data(dataset.data)
@@ -54,7 +51,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <svg height='400px' width='900px'></svg>
+      <svg height='400px' width='880px'></svg>
     );
   }
 }
